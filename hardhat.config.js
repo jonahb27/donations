d0f3d0c7c1899f5f3776f7d08d274beb14543ac9
jonahburian <jonah.burian@gmail.com>
2022-02-06 16:49:45 -0500

    initial contract and scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,46 @@
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require('hardhat-abi-exporter');
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config()
+require('solidity-coverage')
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.0",
+  gasReporter: {
+    enabled: (process.env.REPORT_GAS) ? true : false,
+    currency: 'USD',
+    token: "ETH",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY || null,
+    gasPriceApi: "https://api.etherscan.io/api?module=proxy&action=eth_gasPrice",
+    rst: true,
+    rstTitle: "Gas Usage",
+    showTimeSpent: true,
+    showMethodSig: true
+  },
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.GOERLI_ALCHEMY_API_KEY}`,
+      accounts: [`${process.env.GOERLI_PRIVATE_KEY}`],
+    },
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ROPSTEN_ALCHEMY_API_KEY}`,
+      accounts: [`${process.env.ROPSTEN_PRIVATE_KEY}`],
+    }
+  },
+  abiExporter: {
+    path: './data/abi',
+    runOnCompile: true,
+    clear: true,
+    flat: true,
+    only: ['Agreement'],
+    spacing: 2,
+    pretty: true,
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
